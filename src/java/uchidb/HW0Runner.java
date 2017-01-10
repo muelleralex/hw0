d95317827ff7ffba2c74fad4d17a93181309b72c
Alex Mueller <ajmueller@uchicago.edu>
2017-01-09 22:17:25 -0600

    adding stuff

--- src/java/uchidb/HW0Runner.java
+++ src/java/uchidb/HW0Runner.java
@@ -7,16 +7,17 @@ package uchidb;
 public class HW0Runner {
 
 	//TODO you likely will need to add member variable
-	
+	private static Containers<Integer, String> containers = new alexContainers<Integer, String>();
+
 	// This class is a factory for a singleton containers class.
 	// https://www.tutorialspoint.com/java/java_using_singleton.htm
 	public static Containers<Integer, String> getContainers() {
 		// TODO fix this function
-		return null;
+		return containers;
 	}
 
 
 	public static void main(String[] args){
-		
+
 	}
 }
